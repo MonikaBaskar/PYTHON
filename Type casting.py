ab30310055b569b9c6c50237ef0881eb872a0037
Monika Baskar <baskarmonika175@gmail.com>
2023-09-19 12:37:46 +0530

    add

---
+++ Type casting.py
@@ -0,0 +1,16 @@
+#data type = int,str,boolean,float
+otp = 123456
+print ("your otp is "+ str(otp))# str(otp)- we give temporary change for otp
+print (type(otp))
+#we change otp is string in permanent
+otp = 123456
+otp = str(otp)
+print ("your otp is "+ otp)
+print (type(otp))
+#count string
+count = "20.5"
+print (int(float(count))+1)
+
+
+
+
